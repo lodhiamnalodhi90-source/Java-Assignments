c3e7db8b111f3439bd6f3e0e748e875f30b1ec4b
lodhiamnalodhi90-source <lodhiamnalodhi90@gmail.com>
2026-05-10 01:18:02 +0500

    Add files via upload

---
+++ With Or Without HM/Productwise_HM.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Productwise_HM {
+
+    public static void main(String[] args) {
+
+        File f1 = new File("W:\\Amna\\SalesEVESession.txt");
+        Scanner inp;
+        String dl;
+        HashMap<String,Double> hm = new HashMap<>();
+
+        try {
+            inp = new Scanner(f1);
+            dl = inp.nextLine();
+
+            double qty, amt, uPrice;
+
+
+            while (inp.hasNextLine()) {
+                dl = inp.nextLine();
+                if (dl.trim().length() == 0)continue;
+
+                String[] parts = dl.split("\t");
+                String productName = parts[1];
+                qty = Double.parseDouble(parts[3]);
+                uPrice = Double.parseDouble(parts[2]);
+                amt = qty * uPrice;
+
+
+                if (hm.containsKey(productName))
+                {
+                    amt += hm.get(productName);
+                }
+                hm.put(productName, amt);
+            }
+        }
+
+        catch (Exception e) {
+            System.out.println("Error : " + e.getMessage());
+        }
+
+        System.out.println("Product Wise Sum");
+        for (Map.Entry<String, Double> me : hm.entrySet()){
+            System.out.println(me.getKey() + " --- " + me.getValue());
+        }
+    }
+}
